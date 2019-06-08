7e7b1cf1409b47a342f4884c78b99e2e09c0264d
Vince <15981981193@163.com>
2019-06-08 22:30:03 +0800

    渲染图标

--- info/modules/views.py
+++ info/modules/views.py
@@ -1,6 +1,6 @@
 from info.modules import index_blu
 from info import redis_store
-from flask import render_template
+from flask import render_template, current_app
 
 
 @index_blu.route('/')
@@ -13,4 +13,13 @@ def index():
     redis_store.set("name", "laowang")
     # 向redis中保存一个键值
 
-    return render_template("/index.html")
+    return render_template("index.html")
+
+
+
+# 在打开网页的时候,浏览器会自动向根路径请求图标
+# send_static_file 是flask查找静态文件调用的方法
+@index.route("/favicon.ico")
+def favicon():
+    return current_app.send_static_file("news/favicon.ico")
+# send_static_file: 发送静态文件. 找到项目的静态文件夹中的某个静态文件,并且返回
