ae828a76698cc1ec8eb090b1729a6af95085d684
Vince <15981981193@163.com>
2019-06-08 19:13:19 +0800

    mysql数据库配置信息

--- manage.py
+++ manage.py
@@ -1,7 +1,23 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+class Config(object):
+    """配置信息类"""
+    DEBUG = True
+
+    # 配置mysql信息
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/news_infor"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+# 从类中导入配置信息
+app.config.from_object(Config)
+
+# 获取db
+db = SQLAlchemy(app)
 
 
 @app.route('/')
