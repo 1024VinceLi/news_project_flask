e22c17aee7fa15e47e87efc29677b94ebf40682a
Vince <15981981193@163.com>
2019-06-08 20:15:04 +0800

    抽取项目配置文件

---
+++ config.py
@@ -0,0 +1,28 @@
+import redis
+
+
+class Config(object):
+    """配置信息类"""
+    DEBUG = True  # 开启debug
+
+    # 配置mysql信息
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/news_infor"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 集成redis配置信息
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    # flask_session配置信息
+    SESSION_TYPE = "redis"  # 指定session存储到redis中
+    SESSION_USE_SIGNER = True  # 让cookie中的session_id被加密
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用redis的实例
+    PERMANENT_SESSION_LIFETIME = 86400  # 有效期(单位为秒)
+    # 配置的是session的存储位置
+    """
+    flask 默认是将 session,进行加密,存储到了 cookie 中. 而 Django 默认是将 session 进行加密存
+    储到了数据库中
+    但是都没有存储到 Redis 中方便快速. 所以我们项目中将 session 存储到 redis 中
+
+    """
+    SECRET_KEY = "J12B234B23JAS14r34BJK"  # 设置session在cookie中的id加密的秘钥
--- manage.py
+++ manage.py
@@ -3,48 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 import redis
 from flask_wtf import CSRFProtect  # 导入csrf防御模块
 from flask_session import Session
-app = Flask(__name__)
 from flask_script import Manager  # 导入命令启动模块
-from flask_migrate import  Migrate,MigrateCommand
+from flask_migrate import Migrate, MigrateCommand
+from config import Config
 
 
 
+app = Flask(__name__)
 
 
 manager = Manager(app)  # 创建Manager的实例对象
-
-
-
-
-
-
-class Config(object):
-    """配置信息类"""
-    DEBUG = True
-
-    # 配置mysql信息
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/news_infor"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # 集成redis配置信息
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-
-    # flask_session配置信息
-    SESSION_TYPE = "redis"  # 指定session存储到redis中
-    SESSION_USE_SIGNER = True  # 让cookie中的session_id被加密
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST,port=REDIS_PORT)  # 使用redis的实例
-    PERMANENT_SESSION_LIFETIME = 86400  # 有效期(单位为秒)
-    # 配置的是session的存储位置
-    """
-    flask 默认是将 session,进行加密,存储到了 cookie 中. 而 Django 默认是将 session 进行加密存
-    储到了数据库中
-    但是都没有存储到 Redis 中方便快速. 所以我们项目中将 session 存储到 redis 中
-    
-    """
-    SECRET_KEY = "J12B234B23JAS14r34BJK"  # 设置session在cookie中的id加密的秘钥
-
-
 
 
 # 从类中导入配置信息
